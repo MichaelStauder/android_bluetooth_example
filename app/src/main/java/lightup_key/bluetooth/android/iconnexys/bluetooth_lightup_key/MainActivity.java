31103de6495bc927e06739804fe9391aff4cfbea
admin <stauder79@gmail.com>
2017-12-16 15:31:27 +0100

    initial working demo "green led on at startup" using bluetooth.

--- app/src/main/java/lightup_key/bluetooth/android/iconnexys/bluetooth_lightup_key/MainActivity.java
+++ app/src/main/java/lightup_key/bluetooth/android/iconnexys/bluetooth_lightup_key/MainActivity.java
@@ -1,15 +1,107 @@
 package lightup_key.bluetooth.android.iconnexys.bluetooth_lightup_key;
 
 import android.bluetooth.BluetoothSocket;
-import android.os.ParcelUuid;
+import android.os.Bundle;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.widget.ListView;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import android.app.ListActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.AdapterView.OnItemClickListener;
+
+public class MainActivity extends ListActivity {
+
+    private final static int REQUEST_ENABLE_BT = 1;
+
+    static final List<String> available_bluetooth_devices = new ArrayList<String>();
+    //private final UUID DEFAULT_SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+    private final UUID MY_SSP_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Verify that the device supports Bluetooth
+        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            // Device doesn't support Bluetooth
+        }
+        else {
+            // Verify that the adapter is enabled
+            if (!mBluetoothAdapter.isEnabled()) {
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+            }
+
+            // Verify that the paired device is already available.
+            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+
+            if (pairedDevices.size() > 0) {
+                // There are paired devices. Get the name and address of each paired device.
+                List<String> values = new ArrayList<String>();
+                for (BluetoothDevice device : pairedDevices) {
+                    String deviceName = device.getName();
+                    String deviceHardwareAddress = device.getAddress(); // MAC address
+                    available_bluetooth_devices.add(deviceName);
+
+                    // connect to first paired device (Hack!)
+                    BluetoothSocket socket = null;
+                    try {
+                        mBluetoothAdapter.cancelDiscovery();
+                        socket = device.createInsecureRfcommSocketToServiceRecord(MY_SSP_UUID);
+                        socket.connect();
+                        OutputStream outputStream = socket.getOutputStream();
+                        outputStream.write(49); // ascii 1
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+
+        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_fruit, available_bluetooth_devices));
+
+        ListView listView = getListView();
+        listView.setTextFilterEnabled(true);
+
+        listView.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                // When clicked, show a toast with the TextView text
+                Toast.makeText(getApplicationContext(),
+                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
+    }
+
+}
+/*
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
-import java.util.UUID;
+import android.widget.ListView;
 
-import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
@@ -38,6 +130,7 @@ public class MainActivity extends AppCompatActivity {
 
             if (pairedDevices.size() > 0) {
                 // There are paired devices. Get the name and address of each paired device.
+                List<String> values = new ArrayList<String>();
                 for (BluetoothDevice device : pairedDevices) {
                     String deviceName = device.getName();
                     String deviceHardwareAddress = device.getAddress(); // MAC address
@@ -60,3 +153,4 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 }
+*/

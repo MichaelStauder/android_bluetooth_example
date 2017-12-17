6fef2317aef4b2e6bbf27b2748b4faafd62a4dd0
admin <stauder79@gmail.com>
2017-12-17 12:51:43 +0100

    Refactoring and change background of selected item in ListView.

--- app/src/main/java/lightup_key/bluetooth/android/iconnexys/bluetooth_lightup_key/MainActivity.java
+++ app/src/main/java/lightup_key/bluetooth/android/iconnexys/bluetooth_lightup_key/MainActivity.java
@@ -1,6 +1,8 @@
 package lightup_key.bluetooth.android.iconnexys.bluetooth_lightup_key;
 
+import android.app.Activity;
 import android.bluetooth.BluetoothSocket;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
@@ -14,7 +16,6 @@ import java.util.List;
 import java.util.Set;
 import java.util.UUID;
 
-import android.app.ListActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -22,94 +23,73 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.AdapterView.OnItemClickListener;
 
-public class MainActivity extends ListActivity {
+public class MainActivity extends Activity {
 
     private final static int REQUEST_ENABLE_BT = 1;
 
     static final List<String> available_bluetooth_devices = new ArrayList<String>();
-    //private final UUID DEFAULT_SPP_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
-    private final UUID MY_SSP_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
+    static BluetoothSocket mmSocket;
+    BluetoothDevice mmDevice = null;
+    Set<BluetoothDevice> pairedDevices;
+
+    public void Button1_OnClick(View v){
+        Led_all_off();
+    }
+
+    public void Button2_OnClick(View v){
+        Led_1_On();
+    }
+
+    public void Button3_OnClick(View v){
+        Led_2_On();
+    }
+
+    public void sendBtMsg(String msg2send){
+
+        final UUID MY_SSP_UUID = UUID.fromString("94f39d29-7d6d-437d-973b-fba39e49d4ee");
+
+        if ( mmDevice == null ) {
+           return;
+        }
+
+        try {
+
+            mmSocket = mmDevice.createRfcommSocketToServiceRecord(MY_SSP_UUID);
+            if (!mmSocket.isConnected()){
+                mmSocket.connect();
+            }
+
+            String msg = msg2send;
+            //msg += "\n";
+            OutputStream mmOutputStream = mmSocket.getOutputStream();
+            mmOutputStream.write(msg.getBytes());
+
+            mmSocket.close();
+
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+
+    }
+
+    // candidate: command factory?
+    public void Led_all_off() {
+        sendBtMsg("0");
+    }
+
+    public void Led_1_On() {
+        sendBtMsg("1");
+    }
+
+    public void Led_2_On() {
+        sendBtMsg("2");
+    }
+
 
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-        // Verify that the device supports Bluetooth
-        BluetoothAdapter mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        if (mBluetoothAdapter == null) {
-            // Device doesn't support Bluetooth
-        }
-        else {
-            // Verify that the adapter is enabled
-            if (!mBluetoothAdapter.isEnabled()) {
-                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-            }
-
-            // Verify that the paired device is already available.
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
-
-            if (pairedDevices.size() > 0) {
-                // There are paired devices. Get the name and address of each paired device.
-                List<String> values = new ArrayList<String>();
-                for (BluetoothDevice device : pairedDevices) {
-                    String deviceName = device.getName();
-                    String deviceHardwareAddress = device.getAddress(); // MAC address
-                    available_bluetooth_devices.add(deviceName);
-
-                    // connect to first paired device (Hack!)
-                    BluetoothSocket socket = null;
-                    try {
-                        mBluetoothAdapter.cancelDiscovery();
-                        socket = device.createInsecureRfcommSocketToServiceRecord(MY_SSP_UUID);
-                        socket.connect();
-                        OutputStream outputStream = socket.getOutputStream();
-                        outputStream.write(49); // ascii 1
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }
-
-        setListAdapter(new ArrayAdapter<String>(this, R.layout.list_fruit, available_bluetooth_devices));
-
-        ListView listView = getListView();
-        listView.setTextFilterEnabled(true);
-
-        listView.setOnItemClickListener(new OnItemClickListener() {
-            public void onItemClick(AdapterView<?> parent, View view,
-                                    int position, long id) {
-                // When clicked, show a toast with the TextView text
-                Toast.makeText(getApplicationContext(),
-                        ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
-
-
-            }
-        });
-
-    }
-
-}
-/*
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.content.Intent;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-public class MainActivity extends AppCompatActivity {
-
-    private final static int REQUEST_ENABLE_BT = 1;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -126,7 +106,7 @@ public class MainActivity extends AppCompatActivity {
             }
 
             // Verify that the paired device is already available.
-            Set<BluetoothDevice> pairedDevices = mBluetoothAdapter.getBondedDevices();
+            pairedDevices = mBluetoothAdapter.getBondedDevices();
 
             if (pairedDevices.size() > 0) {
                 // There are paired devices. Get the name and address of each paired device.
@@ -134,23 +114,46 @@ public class MainActivity extends AppCompatActivity {
                 for (BluetoothDevice device : pairedDevices) {
                     String deviceName = device.getName();
                     String deviceHardwareAddress = device.getAddress(); // MAC address
-
-                    // connect to first paired device (Hack!)
-                    BluetoothSocket s;
-                    try {
-                        s = device.createInsecureRfcommSocketToServiceRecord(UUID);
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    try {
-                        s.connect();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                    break;
+                    available_bluetooth_devices.add(deviceName);
                 }
             }
         }
+
+        ListView listv = (ListView) findViewById(R.id.list);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.list_devices, available_bluetooth_devices);
+        listv.setAdapter( adapter );
+
+        listv.setTextFilterEnabled(true);
+        listv.setOnItemClickListener(new OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+            // change all item background to transparant
+            for (int j = 0; j < parent.getChildCount(); j++)
+                parent.getChildAt(j).setBackgroundColor(Color.TRANSPARENT);
+
+            // change the background color of the selected element
+            view.setBackgroundColor(Color.LTGRAY);
+
+            int i = 0;
+            for (BluetoothDevice device : pairedDevices){
+                if ( position == i ) {
+                    mmDevice = device;
+                }
+                ++i;
+            }
+
+            Led_all_off();
+
+            view.setSelected(true);
+
+            // When clicked, show a toast with the TextView text
+            Toast.makeText(getApplicationContext(),
+                    ((TextView) view).getText(), Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+
     }
+
 }
-*/
